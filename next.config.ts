5869ebf7722a389618e3c2af600175f3ec30ca98
Leonardo Franco <leonardojfs83@gmail.com>
2025-06-18 22:48:08 -0300

    manifest json and next-config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
-
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // PWA solo en producción
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -25,5 +29,5 @@ const nextConfig: NextConfig = {
   // }
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
 
